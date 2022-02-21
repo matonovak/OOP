1e68d3d509989981490a387c960300233efcb981
dalybor <xnovakm4@stuba.sk>
2022-02-22 00:33:51 +0100

    hello world

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -3,7 +3,7 @@ package sk.stuba.fei.uim.oop;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!\n");
-        int celeCislo = 1;
+        int celeCislo = Integer.parseInt(args[0]);
         long celeDlheCislo = 1000000000;
         float desatinneCislo = 2.5f;
         double desatinneCisloPlus = 1.444444444;
@@ -20,7 +20,24 @@ public class App {
         float podielFloat = 2/3.0f;
         System.out.println(podielFloat);
 
+        if(celeCislo == 4) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
 
+        for(celeCislo = 0; celeCislo < 5; celeCislo++){
+            switch (celeCislo) {
+                case 1:
+                    System.out.println("case1");
+                    break;
+                case 2:
+                    System.out.println("case2");
+                    break;
+                default:
+                    System.out.println("default");
+            }
+        }
 
     }
 }
