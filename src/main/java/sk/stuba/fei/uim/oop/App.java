814d67b0746560a10523945d9ca59a7f4b906110
dalybor <xnovakm4@stuba.sk>
2022-02-22 00:25:28 +0100

    hello world

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,6 +2,25 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!\n");
+        int celeCislo = 1;
+        long celeDlheCislo = 1000000000;
+        float desatinneCislo = 2.5f;
+        double desatinneCisloPlus = 1.444444444;
+        char znak = 'a';
+        String retazec = "retazec";
+        boolean bool = true;
+
+        int podiel = 2/3;
+        System.out.println(podiel);
+
+        int zvysok = 2%3;
+        System.out.println(zvysok);
+
+        float podielFloat = 2/3.0f;
+        System.out.println(podielFloat);
+
+
+
     }
 }
